c19f26c5e7cfc2e6e6920e93627f3772b2eaa751
isam chajia <isamchajiabeat.com>
2025-05-11 13:22:59 +0100

    solve problem : Generate UUID

---
+++ JavaScript/exercices/generate_uuid.js
@@ -0,0 +1,14 @@
+// Exercise: Generate UUID
+
+// Description: Generate a simple UUID.
+
+function uuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+    const r = Math.random() * 16 | 0;
+    const v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+// Test the function
+console.log(uuid());
