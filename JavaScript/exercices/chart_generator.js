6044186e3f2a88b2be4bf0bf5d376b1fad4ed671
isam chajia <isamchajiabeat.com>
2025-05-11 13:44:58 +0100

    solve problem : Chart Generator

---
+++ JavaScript/exercices/chart_generator.js
@@ -0,0 +1,10 @@
+// Exercise: Chart Generator
+
+// Description: Render chart using a library like Chart.js.
+
+function generateChart(ctx, data) {
+  new Chart(ctx, { type: 'bar', data });
+}
+
+// Test the function
+// generateChart(canvasContext, chartData);
