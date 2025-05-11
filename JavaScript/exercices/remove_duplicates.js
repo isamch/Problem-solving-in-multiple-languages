7f70310bdf39ff9ca8ff92832ff3845bec51f313
isam chajia <isamchajiabeat.com>
2025-05-12 00:50:29 +0100

    solve problem : Remove Duplicates

--- JavaScript/exercices/remove_duplicates.js
+++ JavaScript/exercices/remove_duplicates.js
@@ -7,4 +7,4 @@ function removeDuplicates(arr) {
 }
 
 // Test the function
-console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // Output: [1, 2, 3, 4]
+console.log(removeDuplicates([1, 2, 2, 3])); // Output: [1, 2, 3]
