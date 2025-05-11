a8a742d212a09322c6cbed5740c4e17e5b949341
isam chajia <isamchajiabeat.com>
2025-05-12 00:47:36 +0100

    solve problem : Check Prime

--- JavaScript/exercices/check_prime.js
+++ JavaScript/exercices/check_prime.js
@@ -1,9 +1,9 @@
 // Exercise: Check Prime
 
-// Description: Check if a number is prime.
+// Description: Check if a number is a prime.
 
 function isPrime(n) {
-  if (n < 2) return false;
+  if (n <= 1) return false;
   for (let i = 2; i <= Math.sqrt(n); i++) {
     if (n % i === 0) return false;
   }
