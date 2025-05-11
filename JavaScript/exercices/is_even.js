57aa25d15bbd93a7f7dc5a710fd40a24d3e7b270
isam chajia <isamchajiabeat.com>
2025-05-12 00:55:39 +0100

    solve problem : Is Even

---
+++ JavaScript/exercices/is_even.js
@@ -0,0 +1,10 @@
+// Exercise: Is Even
+
+// Description: Check if a number is even.
+
+function isEven(n) {
+  return n % 2 === 0;
+}
+
+// Test the function
+console.log(isEven(4)); // Output: true
