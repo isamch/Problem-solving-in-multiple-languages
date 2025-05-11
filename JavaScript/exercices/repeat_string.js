ccbdda369937664d2f0f193403182be4bf735829
isam chajia <isamchajiabeat.com>
2025-05-12 00:56:14 +0100

    solve problem : Repeat String

---
+++ JavaScript/exercices/repeat_string.js
@@ -0,0 +1,10 @@
+// Exercise: Repeat String
+
+// Description: Repeat a string a given number of times.
+
+function repeatString(str, times) {
+  return str.repeat(times);
+}
+
+// Test the function
+console.log(repeatString('abc', 3)); // Output: 'abcabcabc'
