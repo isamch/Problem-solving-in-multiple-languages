ad123de35109cb0c0cacab22f7ec8f23ba5cbc12
isam chajia <isamchajiabeat.com>
2025-05-11 13:26:22 +0100

    solve problem : Calculate Area

---
+++ JavaScript/exercices/calculate_area.js
@@ -0,0 +1,10 @@
+// Exercise: Calculate Area
+
+// Description: Calculate area of a rectangle.
+
+function area(w, h) {
+  return w * h;
+}
+
+// Test the function
+console.log(area(5, 3)); // Output: 15
