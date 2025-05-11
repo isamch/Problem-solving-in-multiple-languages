1b3ef01371b5d45fac78e4c334afb314814b3442
isam chajia <isamchajiabeat.com>
2025-05-11 13:27:32 +0100

    solve problem : Decimal to Binary

---
+++ JavaScript/exercices/decimal_to_binary.js
@@ -0,0 +1,10 @@
+// Exercise: Decimal to Binary
+
+// Description: Convert a decimal number to binary.
+
+function decToBin(n) {
+  return n.toString(2);
+}
+
+// Test the function
+console.log(decToBin(10)); // Output: '1010'
