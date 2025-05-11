1cec9f68b920d236372047efe226c65995bf4ffe
isam chajia <isamchajiabeat.com>
2025-05-12 00:53:21 +0100

    solve problem : Reverse String

--- JavaScript/exercices/reverse_string.js
+++ JavaScript/exercices/reverse_string.js
@@ -1,10 +1,10 @@
 // Exercise: Reverse String
 
-// Description: Reverse a given string.
+// Description: Return the reversed version of a given string.
 
 function reverseString(str) {
   return str.split('').reverse().join('');
 }
 
 // Test the function
-console.log(reverseString('hello')); // Output: 'olleh'
+console.log(reverseString('openai')); // Output: 'ianepo'
