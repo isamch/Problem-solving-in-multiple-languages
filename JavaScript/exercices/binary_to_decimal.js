ac910266a55bc1a87b0b9d2a56378f659680bc5d
isam chajia <isamchajiabeat.com>
2025-05-11 13:28:07 +0100

    solve problem : Binary to Decimal

---
+++ JavaScript/exercices/binary_to_decimal.js
@@ -0,0 +1,10 @@
+// Exercise: Binary to Decimal
+
+// Description: Convert a binary string to decimal.
+
+function binToDec(bin) {
+  return parseInt(bin, 2);
+}
+
+// Test the function
+console.log(binToDec('1010')); // Output: 10
