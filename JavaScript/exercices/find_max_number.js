407e5f74be679d64206b6faea69d499b573f48fd
isam chajia <isamchajiabeat.com>
2025-05-12 00:54:30 +0100

    solve problem : Find Max Number

---
+++ JavaScript/exercices/find_max_number.js
@@ -0,0 +1,10 @@
+// Exercise: Find Max Number
+
+// Description: Return the largest number in an array.
+
+function findMax(arr) {
+  return Math.max(...arr);
+}
+
+// Test the function
+console.log(findMax([1, 3, 7, 2])); // Output: 7
