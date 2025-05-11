7e51c0d1100fc5df88516572755ad2faedb6674f
isam chajia <isamchajiabeat.com>
2025-05-12 00:53:55 +0100

    solve problem : Factorial

--- JavaScript/exercices/factorial.js
+++ JavaScript/exercices/factorial.js
@@ -1,9 +1,9 @@
 // Exercise: Factorial
 
-// Description: Calculate the factorial of a number.
+// Description: Return the factorial of a number.
 
 function factorial(n) {
-  if (n === 0) return 1;
+  if (n === 0 || n === 1) return 1;
   return n * factorial(n - 1);
 }
 
