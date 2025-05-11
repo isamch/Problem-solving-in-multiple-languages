88312d43d8693aa240fe0fb656065b8a722df854
isam chajia <isamchajiabeat.com>
2025-05-11 13:10:48 +0100

    solve problem : Temperature Conversion

---
+++ JavaScript/exercices/temperature_conversion.js
@@ -0,0 +1,10 @@
+// Exercise: Temperature Conversion
+
+// Description: Convert Celsius to Fahrenheit.
+
+function cToF(c) {
+  return (c * 9/5) + 32;
+}
+
+// Test the function
+console.log(cToF(0)); // Output: 32
