87b09ff8abb460daf1a982f40620473a5e7fd5e5
isam chajia <isamchajiabeat.com>
2025-05-12 01:02:02 +0100

    solve problem : Flatten Array

--- JavaScript/exercices/flatten_array.js
+++ JavaScript/exercices/flatten_array.js
@@ -1,10 +1,10 @@
 // Exercise: Flatten Array
 
-// Description: Flatten a nested array.
+// Description: Flatten a nested array one level deep.
 
 function flatten(arr) {
-  return arr.flat(Infinity);
+  return arr.reduce((flat, next) => flat.concat(next), []);
 }
 
 // Test the function
-console.log(flatten([1, [2, [3, 4]]])); // Output: [1, 2, 3, 4]
+console.log(flatten([[1, 2], [3, 4]])); // Output: [1, 2, 3, 4]
