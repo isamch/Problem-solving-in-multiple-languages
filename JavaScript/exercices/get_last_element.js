9fb65406319597fb947ebf19100e87679f694810
isam chajia <isamchajiabeat.com>
2025-05-12 01:02:38 +0100

    solve problem : Get Last Element

---
+++ JavaScript/exercices/get_last_element.js
@@ -0,0 +1,10 @@
+// Exercise: Get Last Element
+
+// Description: Return the last element of an array.
+
+function lastElement(arr) {
+  return arr[arr.length - 1];
+}
+
+// Test the function
+console.log(lastElement([1, 2, 3])); // Output: 3
